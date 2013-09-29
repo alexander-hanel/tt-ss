00e4a64ec6bc94ced243103545bd8dadddc56d5c
unknown <alexander.hanel@gmail.com>
2013-09-28 20:20:36 -0600

    first commit

--- api_tokener.py
+++ api_tokener.py
@@ -1,3 +1,33 @@
+'''
+Name:
+        TT&SS
+
+Version:
+        0.1
+        
+Description:
+        Python library for creating text out of API names
+
+Author:
+        alexander<dot>hanel<at>gmail<dot>com
+
+License:
+TT&SS is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see
+<http://www.gnu.org/licenses/>.
+
+'''
+
 import sys
 import re
 #from nltk.corpus import wordnet
